dfad41f0e2e08b65ca43f7744c2230c0a951cf4f
Pylotier <marcosmario2011@gmail.com>
2026-03-03 19:57:58 -0300

    Update Exercicio-5.py

--- Exercicio-5.py
+++ Exercicio-5.py
@@ -10,11 +10,10 @@ A = int(input("Digite o valor do coeficiente A:"))
 B = int(input("Digite o valor do coeficiente B:"))
 C = int(input("Digite o valor do coeficiente C:"))
 
-
 delta = B**2 - 4*A*C
 delta = delta**0.5
-print(delta)
 resPositivo = (-B+delta)/ (2*A)
 resNegativo = (-B-delta)/ (2*A)
-print("Resultado positivo:", resPositivo, "e negativo:", resNegativo)
+print("Resultado do x1 (positivo):", resPositivo)
+print("Resultado do x2 (negativo):", resNegativo)
 #Fim
